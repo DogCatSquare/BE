5b5bd0dd5e4f159d102fab7e27c9264619b9b5dd
bikooju <jhjk1234@gmail.com>
2025-01-07 13:36:42 +0900

    초기세팅

---
+++ src/test/java/DC_square/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package DC_square.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
