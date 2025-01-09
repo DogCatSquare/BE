9313cc7d0ab0a172ab0c0b03558a5e04fd82b66b
bikooju <jhjk1234@gmail.com>
2025-01-09 19:46:55 +0900

    세팅

--- src/main/java/DC_square/spring/config/SecurityConfig.java
+++ src/main/java/DC_square/spring/config/SecurityConfig.java
@@ -1,2 +1,41 @@
-package DC_square.spring.config;public class SecurityConfig {
-}
+//package DC_square.spring.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@EnableWebSecurity //Spring security 설정을 활성화 => 직접 작성한 보안 설정이 기본설정보다 더 높은 우선순위
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((requests) -> requests
+//                        .requestMatchers("/", "/home", "/signup", "/members/signup", "/css/**").permitAll()
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin((form) -> form
+//                        .loginPage("/login")
+//                        .defaultSuccessUrl("/home", true)
+//                        .permitAll()
+//                )
+//                .logout((logout) -> logout
+//                        .logoutUrl("/logout")
+//                        .logoutSuccessUrl("/login?logout")
+//                        .permitAll()
+//                )
+//                .oauth2Login(oauth2 -> oauth2
+//                        .loginPage("/login")
+//                        .defaultSuccessUrl("/home", true)
+//                        .permitAll()
+//                );
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
--- src/main/java/DC_square/spring/config/SwaggerConfig.java
+++ src/main/java/DC_square/spring/config/SwaggerConfig.java
@@ -1,4 +1,35 @@
 package DC_square.spring.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.servers.Server;
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI UMCstudyAPI() {
+        Info info = new Info()
+                .title("댕냥 스퀘어 API")
+                .description("댕냥 스퀘어 API 명세서")
+                .version("1.0.0");
+
+        String jwtSchemeName = "JWT TOKEN";
+//        // API 요청헤더에 인증정보 포함
+//        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(info)
+//                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
 }
