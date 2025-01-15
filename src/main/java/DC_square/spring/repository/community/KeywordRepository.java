2bedfd283dc0b139a0f4a2bafe53ea28070533f6
bikooju <jhjk1234@gmail.com>
2025-01-15 17:03:55 +0900

    게시판 생성,조회 구현

--- src/main/java/DC_square/spring/repository/community/KeywordRepository.java
+++
@@ -1,9 +0,0 @@
-package DC_square.spring.repository.community;
-
-import DC_square.spring.domain.entity.community.Keyword;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface KeywordRepository extends JpaRepository<Keyword, Long> {
-}
