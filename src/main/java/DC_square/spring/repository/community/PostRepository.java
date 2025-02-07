1a95688f55fbf1b083660f74a9fb20f0a4b31a6c
bikooju <jhjk1234@gmail.com>
2025-02-07 21:28:11 +0900

    게시판 검색기능, 게시글들 조회

--- src/main/java/DC_square/spring/repository/community/PostRepository.java
+++ src/main/java/DC_square/spring/repository/community/PostRepository.java
@@ -3,5 +3,9 @@ package DC_square.spring.repository.community;
 import DC_square.spring.domain.entity.community.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+
 public interface PostRepository extends JpaRepository<Post,Long> {
+    List<Post> findByBoardId(Long boardId);
 }
