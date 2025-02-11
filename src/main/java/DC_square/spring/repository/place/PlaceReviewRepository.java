eb7dcdd4308252f8bee1fbf0f21205eed1eea088
whiteys1 <whiteys1@naver.com>
2025-02-11 22:03:30 +0900

    마이 리뷰 조회 api 구현

--- src/main/java/DC_square/spring/repository/place/PlaceReviewRepository.java
+++ src/main/java/DC_square/spring/repository/place/PlaceReviewRepository.java
@@ -1,11 +1,15 @@
 package DC_square.spring.repository.place;
 
 import DC_square.spring.domain.entity.place.PlaceReview;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PlaceReviewRepository extends JpaRepository<PlaceReview, Long> {
     List<PlaceReview> findAllByPlaceId(Long placeId);
 
+    @Query("SELECT pr FROM PlaceReview pr WHERE pr.user.id = :userId")
+    List<PlaceReview> findAllByUserId(@Param("userId") Long userId);
 }
