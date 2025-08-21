982435e8cea1c88f76f8da27f97f4cd7b69a3b12
Gabin Kim <gabin321321@gmail.com>
2025-08-21 13:35:50 +0900

    0821

--- src/main/java/DC_square/spring/domain/entity/place/PlaceImage.java
+++ src/main/java/DC_square/spring/domain/entity/place/PlaceImage.java
@@ -16,7 +16,7 @@ public class PlaceImage {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "photo_reference", nullable = false)
+    @Column(name = "photo_reference", nullable = false, length = 2000)
     private String photoReference;
 
     @Column(name = "width")
